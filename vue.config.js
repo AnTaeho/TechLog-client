7c6a0b8f7fc2c57c4d2a5c33817120e162aedb18
Taaate <an981022@naver.com>
2024-09-05 11:51:27 +0900

    도전

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,19 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  lintOnSave: false
+  lintOnSave: false,
+  devServer: {
+    proxy: {
+      '/posts': {
+        target: 'https://api.jobava.online',  // API 서버 URL
+        changeOrigin: true,
+        pathRewrite: { '^/posts': '' }  // 필요에 따라 경로 수정
+      },
+      '/tags': {
+        target: 'https://api.jobava.online',  // API 서버 URL
+        changeOrigin: true,
+        pathRewrite: { '^/tags': '' }  // 필요에 따라 경로 수정
+      }
+    }
+  }
 })
